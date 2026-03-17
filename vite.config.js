fc4ca0dc2306dabd1cd44d40bc0329cf702a24b7
Redzer <redzerriley@gmail.com>
2026-03-17 19:51:18 +0800

    Added something in the vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This matches your GitHub repository name exactly
+  base: '/RedzerMonsod-Portfolio-Website/',
 });
